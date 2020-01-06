a35763bdb8a4247a4a2676c0c9ee384d38245e94
Sr Full Stack Dev Muhammad Tariq <tariq.zameen.com@gmail.com>
2020-01-06 19:38:38 +0500

    Lecture # 17 about ORM Eloquent

---
+++ app/Http/Controllers/ORMController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UsersModel;
+use App\Models\BooksModel;
+
+class ORMController extends Controller
+{
+    function orm_index()
+    {
+
+    	$orm = new UsersModel();
+
+    	echo "<pre>";
+
+    	
+  
+    	//print_r($orm);
+    	
+    	
+
+    	//$data = $orm->all();
+
+    	//$data = $orm->where(["id"=> 1])->get();
+
+    	//$data = $orm->orwhere(["id"=> 1 ,"username"=>""])->get();
+    	//$data = $orm->first();
+    	
+    	//$data = $orm->all();
+
+    	//print_r($orm->find(1));
+    	//print_r($orm->findOrFail(10));
+
+    	/*
+    	
+    	foreach ($data as $UsersModel) {
+    		//print_r($UsersModel);
+    		echo $UsersModel->name."<br/>";
+    	}*/
+    	
+    	//dd($data);
+
+
+    	//save //
+    	
+
+    	
+    	/*
+    	$orm->name="Usama & Shahid";
+    	$orm->username="shahid@gmail.com";
+    	$orm->password ="121";
+	
+		//return type is boolean
+    	var_dump($orm->save());
+		*/
+
+    	//update//
+    	
+    	/*
+    	$data = $orm->find(1);
+    	$data->username = "xyz@gmail.com";
+    	var_dump($data->save());
+		
+		*/
+
+
+    	/*
+		//remove//
+
+		$data = $orm->find(1);
+		var_dump($data->delete());
+		*/
+
+		//Class Work done by Students
+		//06-Jan-2020
+
+		$book = new BooksModel();
+
+		//$book->name = "xyz";
+		//$book->save();
+		$data = $book->all();
+		echo "<pre>";
+		print_r($data);
+
+
+
+
+		//end 06-Jan-2020 //
+
+
+
+
+    }
+}
